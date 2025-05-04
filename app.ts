d52efab7dbed4019097916bee23b6729cb365d5d
Еремей Калеев <kaleevwork@mail.ru>
2025-05-04 09:21:43 +0300

    chore(app.ts): "make app"
    finished my first homework

--- app.ts
+++ app.ts
@@ -1,5 +1,16 @@
-const totalPrice = ({...props}) => {
-  
+interface PriceOptions {
+  price: number;
+  discount: number;
+  isInstallment: boolean;
+  months: number;
+}
+const totalPrice = ({...props}: PriceOptions): number => {
+  const discountAmount = props.price * (props.discount / 100);
+  const finalPrice = props.price - discountAmount;
+  if (props.isInstallment) {
+    return finalPrice / props.months;
+  }
+  return finalPrice;
 };
 
 const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
